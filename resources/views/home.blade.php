b4be2dac56bc49129ecad999fae3c6596c26a364
ryanf10 <ryanfernaldy10@gmail.com>
2020-07-19 14:47:11 +0700

    Create Login,Register,Home using middleware auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    <div class="card">
+                        <div class="card-header">
+                            Featured
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Forum</h5>
+                            <p class="card-text">Let's discuss Together</p>
+                            <a href="#" class="btn btn-primary">Enter Forum</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
