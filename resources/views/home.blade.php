2af4d1fc1e7bd2ceaf8de9e055d6f4954a613359
KresnaAP <kresnaadhipramana5758@gmail.com>
2020-07-20 14:22:11 +0700

    minor fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Featured') }}</div>
 
                 <div class="card-body">
-                    <div class="card">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Forum</h5>
-                            <p class="card-text">Let's discuss Together</p>
-                            <a href="{{url('/forum')}}" class="btn btn-primary">Enter Forum</a>
-                        </div>
-                    </div>
+                    <h5 class="card-title">Forum</h5>
+                    <p class="card-text">Let's discuss Together</p>
+                    <a href="{{url('/forum')}}" class="btn btn-primary">Enter Forum</a>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+    <div class="card border-0 shadow my-5">
+        <div class="card-body p-5">
+            <h1 class="font-weight-light mb-4">Online Discussion Forum</h1>
+            <p class="lead">
+                This forums can be used for many purposes, such as helping students to review material prior to an assignment or exam, engaging students in discussion of course material before coming to class, and reflecting on material that they have read or worked with outside of class.
+            </p>
+        </div>
+    </div>
+</div>
 @endsection
